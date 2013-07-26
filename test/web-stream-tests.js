4eb80433da8745d7e9528b49d4cfe0a291eeddf4
Eric Lee <eric.lee@exacttarget.com>
2013-07-26 15:44:26 -0700

    tests :tropical_fish:

---
+++ test/web-stream-tests.js
@@ -0,0 +1,35 @@
+var should = require('should');
+var WebStream = require('..');
+
+describe('#ctor', function() {
+  describe('options argument', function() {
+    it('can be omitted', function() {
+      should.exist(new WebStream(), 'should allow zero argument constructor');
+    });
+    describe('attributes', function() {
+      it('can specify a maximum backoff time', function() {
+        var arg = {
+          maxBackoff : 1000
+        };
+        new WebStream(arg).should.have.property('maxBackoff', 1000);
+      });
+      it('can specify a retry strategy', function() {
+        var arg = {
+          shouldRetry : function(err, res, body) { return !!err; }
+        };
+        new WebStream(arg).shouldRetry.should.be.an.instanceOf(Function);
+      });
+    });
+  });
+});
+
+describe('input object', function() {
+  it.skip('specifies a url attribute', function() {
+    new Readable({objectMode : true});
+  });
+});
+
+describe('output object', function() {
+  it.skip('specifies a response', function() {
+  });
+});
